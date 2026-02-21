e89bb9ebe13fd185e26f3310142e03fdcbba5b9d
Kyle Ton <kyleton06@gmail.com>
2026-02-20 16:40:53 -0800

    p02 completed

--- company-specific/okta/sre-intern-2026/2-Technical-Prep/p02_top_k_ips/solution.py
+++ company-specific/okta/sre-intern-2026/2-Technical-Prep/p02_top_k_ips/solution.py
@@ -7,5 +7,31 @@ def top_k_ips(filepath: str, k: int) -> List[str]:
     """
     Return top-k IPs by frequency (desc), tie by ip (asc).
     Invalid lines ignored as described in README.
+
+    We know:
+    timestamp ip endpoint, we need k most freq. ip addresses
     """
-    raise NotImplementedError
+    import heapq
+    ip_dict = {}
+    with open(filepath, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3 and parts[1] != "":
+                ip = parts[1].strip()
+                ip_dict[ip] = ip_dict.get(ip, 0) + 1
+    
+    heap = []
+    heapq.heapify(heap)
+    for ip_addr, count in ip_dict.items():
+        heapq.heappush(heap, (-count, ip_addr))
+    
+    result = []
+    while len(result) < k:
+        if heap:
+            item = heapq.heappop(heap)
+            print(item)
+            result.append(item[1])
+        else:
+            break
+    
+    return result
