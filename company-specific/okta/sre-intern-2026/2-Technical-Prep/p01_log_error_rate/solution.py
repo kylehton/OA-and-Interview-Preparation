b87cccf23701a77ad75109997cefc42b1da6aa78
Kyle Ton <kyleton06@gmail.com>
2026-02-20 18:56:56 -0800

    pasted solved ans. back in

--- company-specific/okta/sre-intern-2026/2-Technical-Prep/p01_log_error_rate/solution.py
+++ company-specific/okta/sre-intern-2026/2-Technical-Prep/p01_log_error_rate/solution.py
@@ -9,8 +9,26 @@ def compute_error_rate(filepath: str) -> Dict[str, float]:
 
     See README for line format and invalid-line rules.
     Must stream the file line-by-line.
+
+    So we know:
+    - formatting is timestamp service status
+    - need to read in per line
     """
-    raise NotImplementedError
+    error_dict = {} # 0 -> total req, 1 -> total error
+    with open(filepath, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3 and parts[2].isnumeric() and parts[1] != "":
+                service = parts[1].strip()
+                status = int(parts[2].strip())
+                if service not in error_dict:
+                    error_dict[service] = [0, 0]
+                error_dict[service][0] += 1
+                if status >= 500:
+                    error_dict[service][1] += 1
+    for service, nums in error_dict.items():
+        error_dict[service] = round((nums[1]/nums[0]), 4)
+    return error_dict
 
 
 def services_by_error_rate(filepath: str) -> List[Tuple[str, float]]:
@@ -20,4 +38,22 @@ def services_by_error_rate(filepath: str) -> List[Tuple[str, float]]:
     - total_requests desc
     - service asc
     """
-    raise NotImplementedError
+    error_dict = {} # 0 -> total req, 1 -> total error
+    with open(filepath, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3 and parts[2].isnumeric() and parts[1] != "":
+                service = parts[1].strip()
+                status = int(parts[2].strip())
+                if service not in error_dict:
+                    error_dict[service] = [0, 0]
+                error_dict[service][0] += 1
+                if status >= 500:
+                    error_dict[service][1] += 1
+    error_dict = sorted(error_dict.items(), key=lambda x : (-round(x[1][1]/x[1][0], 4), -x[1][0], x[0]))
+    result = []
+    for item in error_dict:
+        result.append((item[0], round((item[1][1]/item[1][0]), 4)))
+
+    return result
+
