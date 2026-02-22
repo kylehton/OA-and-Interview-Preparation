138c79ff053c3437f2dcd821ddd6dc69ac70cf7d
Kyle Ton <kyleton06@gmail.com>
2026-02-21 19:40:58 -0800

    p06 completed

--- company-specific/okta/sre-intern-2026/2-Technical-Prep/p06_retry_backoff/solution.py
+++ company-specific/okta/sre-intern-2026/2-Technical-Prep/p06_retry_backoff/solution.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import Callable, TypeVar
+from typing import Callable, Type, TypeVar
 import time
 
 T = TypeVar("T")
 
+# RuntimeError, TypeError, ValueError
 
 def retry(
     operation: Callable[[], T],
@@ -12,8 +13,20 @@ def retry(
     base_delay: float,
     sleep_fn: Callable[[float], None] = time.sleep,
 ) -> T:
-    raise NotImplementedError
-
+    prev_exception = None
+    attempts = 0
+    while (attempts <= retries):
+        try:
+            return_val = operation()
+            return return_val
+        except Exception as e:
+            prev_exception = e
+            if attempts == retries:
+                raise ValueError(e)
+            else:
+                sleep_fn(base_delay * (2 ** attempts))
+                attempts += 1
+    return prev_exception
 
 def retry_with_jitter(
     operation: Callable[[], T],
@@ -22,4 +35,17 @@ def retry_with_jitter(
     jitter_fn: Callable[[], float],
     sleep_fn: Callable[[float], None] = time.sleep,
 ) -> T:
-    raise NotImplementedError
+    prev_exception = None
+    attempts = 0
+    while (attempts <= retries):
+        try:
+            return_val = operation()
+            return return_val
+        except Exception as e:
+            prev_exception = e
+            if attempts == retries:
+                raise ValueError(e)
+            else:
+                sleep_fn(base_delay * (2 ** attempts) + jitter_fn())
+                attempts += 1
+    return prev_exception
