4e9481a6943629ce23edf651a64ed83894327850
Kyle Ton <kyleton06@gmail.com>
2026-02-23 11:15:12 -0800

    p10 completed

--- company-specific/okta/sre-intern-2026/2-Technical-Prep/p10_stream_unique_users/solution.py
+++ company-specific/okta/sre-intern-2026/2-Technical-Prep/p10_stream_unique_users/solution.py
@@ -4,8 +4,26 @@ from typing import List, Tuple
 
 
 def count_unique_users(filepath: str) -> int:
-    raise NotImplementedError
-
+    seen = set()
+    count = 0
+    with open(filepath, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3:
+                user = parts[1]
+                if user not in seen:
+                    seen.add(user)
+                    count += 1
+    return count
 
 def top_active_users(filepath: str, k: int) -> List[Tuple[str, int]]:
-    raise NotImplementedError
+    from collections import defaultdict
+    import heapq
+    user_dict = defaultdict(int)
+    with open(filepath, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3:
+                user_dict[parts[1]] += 1
+    
+    return heapq.nsmallest(k, user_dict.items(), key=lambda x: (-x[1], x[0]))
