f342a7e5977dc3abfff0f123c05708df63f09331
Kyle Ton <kyleton06@gmail.com>
2026-02-22 18:38:26 -0800

    p08 completed

--- company-specific/okta/sre-intern-2026/2-Technical-Prep/p08_p95_latency/solution.py
+++ company-specific/okta/sre-intern-2026/2-Technical-Prep/p08_p95_latency/solution.py
@@ -4,8 +4,26 @@ from typing import Dict, List, Tuple
 
 
 def p95_latency(filepath: str) -> Dict[str, int]:
-    raise NotImplementedError
+    from collections import defaultdict
+    import math
+
+    services = defaultdict(list)
+    with open(filepath, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 2 and parts[1].isnumeric() and int(parts[1]) >= 0:
+                services[parts[0]].append(int(parts[1]))
+
+    result = {}
+    for service, latencies in services.items():
+        latencies.sort()
+        result[service] = services[service][math.ceil(0.95 * len(latencies)) - 1]
+    
+    return result
 
 
 def top_p95_services(filepath: str, k: int) -> List[Tuple[str, int]]:
-    raise NotImplementedError
+    import heapq
+    all_p95 = p95_latency(filepath)
+    return heapq.nsmallest(k, all_p95.items(), key=lambda x: (-x[1], x[0]))
+    
